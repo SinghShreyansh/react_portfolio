f882fdd3e9ec1e51e5e16f3df475e3f87c01acb0
Shreyansh Singh <91277635+SinghShreyansh@users.noreply.github.com>
2023-10-19 21:35:33 +0530

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -41,6 +41,10 @@ const Header = () => {
                 <img src={Twitter} alt="" />
             
                 </a>
+    <a className="exam" href='https://chat.openai.com/share/7efdc44b-fa03-4e09-9c86-c98ab6a50710'>
+                <img src={Twitter} alt="" />
+            
+                </a>
             <a className="Github" href='https://github.com/SinghShreyansh' >
                 <img src= {Github} alt="" />
            
